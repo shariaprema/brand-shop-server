55f21270dc2dc69b1f38bf175ee2f143aa3067b7
shariaprema <shariaprema@gmail.com>
2023-10-21 10:06:45 +0600

    create brands collection using get method for products and brands

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5001;
@@ -8,8 +9,10 @@ const port = process.env.PORT || 5001;
 app.use(cors())
 app.use(express.json())
 
+// const uri = "mongodb://localhost:27017"
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wttgumh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
-const uri = "mongodb+srv://shariaprema123:wQlP9pQCTMdhKId4@cluster0.wttgumh.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,8 +28,9 @@ async function run() {
     await client.connect();
 
     const productCollection = client.db("productDB").collection("products");
+    const brandCollection = client.db("productDB").collection("brands");
 
-    //POST
+    //POST :create product
     app.post("/products", async (req, res) => {
       const product = req.body;
       console.log('Product',product);
@@ -34,7 +38,27 @@ async function run() {
       console.log(result);
       res.send(result);
     });
-    
+
+
+    //GET :Display all products
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    //GET :Display all brands
+    app.get("/brands", async (req, res) => {
+      const result = await brandCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
 
 
 
