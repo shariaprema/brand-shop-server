b0b5ee87fefba18acd0e59cb4d898b6dc97d312b
shariaprema <shariaprema@gmail.com>
2023-10-24 20:12:28 +0600

    create cart collection and complete post, get and delete operation for Cart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5001;
 
@@ -29,6 +29,7 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("products");
     const brandCollection = client.db("productDB").collection("brands");
+    const cardCollection = client.db("productDB").collection("cart");
 
     //POST :create product
     app.post("/products", async (req, res) => {
@@ -40,6 +41,16 @@ async function run() {
     });
 
 
+    //POST :create cart
+    app.post("/cart", async (req, res) => {
+      const cart = req.body;
+      console.log('cart',cart);
+      const result = await cardCollection.insertOne(cart);
+      console.log(result);
+      res.send(result);
+    });
+
+//-----------------------------------------------------------------
     //GET :Display all products
 
     app.get("/products", async (req, res) => {
@@ -47,23 +58,87 @@ async function run() {
       res.send(result);
     });
 
+
+    // individual products brand:
+    app.get("/products/:brandName", async (req, res) => {
+      const brandName = req.params.brandName;
+      const query = { brandName: (brandName)};
+      const cursor = productCollection.find(query)
+      const result = await cursor.toArray();
+      console.log('check-------->',result);
+      res.send(result);
+    });
+
+
+
+    // individual products ID:
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("id", id);
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await productCollection.findOne(query);
+      console.log('--->',result);
+      res.send(result);
+    });
+
+
+
+//-------------------------------------------------
+
+
     //GET :Display all brands
     app.get("/brands", async (req, res) => {
       const result = await brandCollection.find().toArray();
       res.send(result);
     });
 
+    
 
 
+    app.get("/brands/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      console.log(brand);
+      const query = {
+        brand: (brand),
+      };
+      const cursor = brandCollection.find(query)
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
 
 
+    app.get("/brands/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await brandCollection.findOne().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+//-----------------------------------------------------
+//GET for Cart 
+app.get("/cart", async (req, res) => {
+  const cursor = cardCollection.find()
+  const carts = await cursor.toArray();
+  res.send(carts);
+});
+
+//---------------------------------------
+// DELETE cart Operation
+app.delete("/cart/:id", async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await cardCollection.deleteOne(query)
+})
 
 
-
-
-
-
-
+//-------------------------------------------------
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
